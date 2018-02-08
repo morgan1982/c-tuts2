8ba3db10c844b13c45e2c86dd5b9a4a5e903bc52
morgan1982 <alexandrisanas@gmail.com>
2018-02-08 21:47:40 +0200

    init

---
+++ test.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+
+using namespace std;
+
+int main ()
+{
+    int age;
+    int totalAge = 0;
+    int numberOfPeople = 0;
+    int average;
+
+    cout << "enter age or -1 to exit" << endl;
+    cin >> age;
+
+    while(age != -1) {
+        totalAge = totalAge + age;
+        numberOfPeople++;
+
+        cout << "enter age or -1 to quit" << endl;
+        cin >> age;
+    }
+    average = totalAge / numberOfPeople;
+    cout << "the average is: " << average << endl;
+
+
+
+    return 0;
+}
+
