72ba4d6925e2cfc4c9a403e7aa590451bc27b1ab
morgan1982 <alexandrisanas@gmail.com>
2018-02-11 20:20:56 +0200

    add time

--- test.cpp
+++ test.cpp
@@ -1,28 +1,20 @@
 #include <iostream>
-
+#include <cstdlib>
+#include <ctime>
 
 using namespace std;
 
+
 int main ()
 {
-    int age;
-    int totalAge = 0;
-    int numberOfPeople = 0;
-    int average;
 
-    cout << "enter age or -1 to exit" << endl;
-    cin >> age;
+    srand(time(0));
 
-    while(age != -1) {
-        totalAge = totalAge + age;
-        numberOfPeople++;
-
-        cout << "enter age or -1 to quit" << endl;
-        cin >> age;
+    for (int x = 1; x < 25; x++ ) {
+        cout
+             << 1 + (rand() % 6)
+             ;
     }
-    average = totalAge / numberOfPeople;
-    cout << "number of people: " << numberOfPeople << endl;
-    cout << "the average is: " << average << endl;
 
 
 
